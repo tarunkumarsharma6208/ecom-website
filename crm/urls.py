ac1746958a116a2d6ac7f81c7ccb7bd34dc9a477
tarunkumarsharma6208 <51085860+tarunkumarsharma6208@users.noreply.github.com>
2020-08-01 20:38:05 +0530

    ecom

---
+++ crm/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+
+    path('customer/<str:pk_test>/', views.customer, name='customer'),
+    path('create_order/<str:pk>/', views.create_order, name='create_order'),
+    path('update_order/<str:pk>/', views.update_order, name='update_order'),
+    path('delete_order/<str:pk>/', views.delete_order, name='delete_order'),
+
+    path('login/', views.loginPage, name='loginPage'),
+    path('register/', views.registerPage, name='registerPage'),
+    path('logout/', views.logoutUser, name='logoutUser'),
+
+    path('user/', views.userPage, name='userPage'),
+    path('account/', views.accountSettings, name='accountSettings'),
+
+]
---
+++ crm/views.py
@@ -0,0 +1,150 @@
+from django.contrib.auth.models import Group
+from django.forms import inlineformset_factory
+from django.shortcuts import render, redirect
+from .models import Product, Order, Customer
+from .forms import OrderForm, CreateUserForm, CustomerForm
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users, admin_only
+
+
+@unauthenticated_user
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            messages.success(request, 'Account was created for: ' + username)
+            return redirect('loginPage')
+    context = {'form': form}
+    return render(request, 'crm/register.html', context)
+
+
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    context = {}
+    return render(request, 'crm/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('loginPage')
+
+
+@login_required(login_url='loginPage')
+@admin_only
+def home(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
+               'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+    return render(request, 'crm/dashboard.html', context)
+
+
+# user page function starting here
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['customer'])
+def userPage(request):
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    print('ORDERS:', orders)
+    context = {'orders': orders, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+    return render(request, 'crm/user.html', context)
+
+
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'crm/account_settings.html', context)
+
+
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['admin'])
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'crm/products.html', {'products': products})
+
+
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['admin'])
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count,
+               'myFilter': myFilter}
+    return render(request, 'crm/customer.html', context)
+
+
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['admin'])
+def create_order(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    if request.method == 'POST':
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+    context = {'formset': formset}
+    return render(request, 'crm/order_form.html', context)
+
+
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['admin'])
+def update_order(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'crm/order_form.html', context)
+
+
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['admin'])
+def delete_order(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {'item': order}
+    return render(request, 'crm/delete.html', context)
